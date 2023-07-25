b459424cce05131920049f3dfebf1b2446f996dc
Aaron Moore <aaron.mv.moore@gmail.com>
2023-07-25 12:01:49 -0400

    Add clean gdf functions

--- prepare.py
+++ prepare.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from acquire import get_mac_data
+from pathlib import Path
+from acquire import get_mac_data, get_census_data
 
 def clean_mac_data():
     '''
@@ -38,3 +39,73 @@ def clean_mac_data():
     df['close_time'] = df['closed'] - df['created']
 
     return df
+
+def get_clean_gdf10(gdf10):
+    '''
+    This function drops unnecessary columns, edits names, and changes values in 2 columns
+    Modules:
+        from pathlib import Path
+        import pandas as pd
+    '''
+
+    # create file path
+    data_path = Path('./data')
+    file_path = data_path.joinpath('Census_Block_Boundaries_2010_Clean.csv')
+
+    # check for clean data file 
+    if file_path.exists():
+
+        # return the clean data
+        return pd.read_csv(file_path, index_col=0)
+
+    # drop columns
+    cols_to_drop = ['fid', 'uace10', 'uatype', 'statefp10', 'ur10','funcstat10','mtfcc10','intptlon10','intptlat10']
+    gdf10.drop(cols_to_drop, axis=1, inplace=True)
+
+    # strip unecesasry values and change feature name
+    gdf10.loc[:,'name10'] = gdf10.loc[:,'name10'].str.strip('Block ')
+    gdf10.rename({'name10':'blockname10'}, axis=1, inplace=True)
+
+    # lowercase columns and remove extra '_'
+    gdf10.columns = gdf10.columns.str.lower().str.replace('__', '_')
+
+    gdf10.to_csv(file_path)
+                                          
+    return gdf10
+
+    
+
+
+def get_clean_gdf20(gdf20):
+    '''
+    This function drops unnecessary columns, edits names, and changes values in 2 columns
+    Modules:
+        from pathlib import Path
+        import pandas as pd
+    '''
+
+    # create file path
+    data_path = Path('./data')
+    file_path = data_path.joinpath('Census_Block_Boundaries_2020_Clean.csv')
+
+    # check for clean data file 
+    if file_path.exists():
+
+        # return the clean data
+        return pd.read_csv(file_path, index_col=0)
+
+    # drop columns
+    cols_to_drop = ['fid', 'uace20', 'uatype20', 'statefp20', 'ur20','funcstat20','mtfcc20','intptlon20','intptlat20']
+    gdf20.drop(cols_to_drop, axis=1, inplace=True)
+
+    # strip unecesasry values and change feature name
+    gdf20.loc[:,'name20'] = gdf20.loc[:,'name20'].str.strip('Block ')
+    gdf20.rename({'name20':'blockname20'}, axis=1, inplace=True)
+
+    # lowercase columns and remove extra '_'
+    gdf20.columns = gdf20.columns.str.lower().str.replace('__', '_')
+
+    gdf20.to_csv(file_path)
+                                          
+    return gdf20
+    
