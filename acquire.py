f8a228cfdef05b85da7da77c78f38cdd6a69b67a
Aaron Moore <aaron.mv.moore@gmail.com>
2023-07-25 12:01:20 -0400

    Change geojson to csv for fast load

--- acquire.py
+++ acquire.py
@@ -1,19 +1,19 @@
 import pandas as pd
-import geopandas
 
 def get_census_data():
     '''
     This module acquires the census block identifiers and geometry for the 2020 and 2010 Deciennieal Census 
     Modules:
-        import geopandas
+        import pandas as pd
     '''
     # acquire the data from 2010
-    gdf10 = geopandas.read_file('data/Census_Block_Boundaries_2010.geojson')
+    gdf10 = pd.read_csv('data/Census_Block_Boundaries_2010.csv')
 
     # acquire the census blocks from 2020
-    gdf20 = geopandas.read_file('data/Census_Block_Boundaries_2020.geojson')
+    gdf20 = pd.read_csv('data/Census_Block_Boundaries_2020.csv')
     
     return gdf10, gdf20
+
 
 
 # creating a function to acquire the raw dfata
