5a5c5d8f682a4fd5e8e0d21a47757e517ac54e58
Aaron Moore <aaron.mv.moore@gmail.com>
2023-08-08 12:46:17 -0400

    Add 2020 diciennial data and labels functions

--- acquire.py
+++ acquire.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 def get_census_data():
     '''
@@ -25,5 +26,40 @@ def get_mac_data():
     '''
     # read the file
     df = pd.read_csv('data/Mayor_s_Action_Center_Service_Cases.csv')
+
+    return df
+
+
+def get_2020_census_data():
+    '''
+    This function gets 2020 deciennial data from the census bureau api website for all Zip Code Tabulation Areas (ZCTAs) in the state of Indiana
+    Modules:
+        import pandas as pd
+        import requests
+    '''
+    # assign url
+    url = 'https://api.census.gov/data/2020/dec/dp?get=group(DP1)&for=zip%20code%20tabulation%20area%20(or%20part):*&in=state:18'
+
+    # get data and turn into json
+    r = requests.get(url).json()
+
+    # create data frame
+    df = pd.DataFrame(r[1:], columns=r[0])
+
+    return df
+
+
+def get_2020_census_labels():
+    '''
+    This function pulls that labels associated woith th column names for the 2020 diciennial census data
+    '''
+    # assign url
+    url = 'https://api.census.gov/data/2020/dec/dp/groups/DP1/'
+
+    # get data and turn into json
+    r = requests.get(url).json()
+
+    # convert to df
+    df = pd.DataFrame(r['variables']).T
 
     return df
